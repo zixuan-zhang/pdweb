f4a22ed30e4fcf62a0413c82a84398976e7ec1d8
zixuan-zhang <zixuan.zhang.victor@gmail.com>
2015-03-22 20:52:36 +0800

    temporate save. trying to test image display plugin with jquery

---
+++ pidiao/uploader.py
@@ -0,0 +1,51 @@
+"""
+Created on 3.17 2015
+
+@Author: Zhang Zixuan(zixuan.zhang.victor@gmail.com)
+
+This script provide API to upload files
+
+"""
+
+from qiniu import Auth
+from qiniu.services.storage.uploader import put_data, put_file
+
+import pymongo
+
+class Qiniu:
+    
+    def __init__(self, bucket_name="pd-picture"):
+        self.ACCESS_KEY = "UNipA1LL6APPZ5w_LAOzWXGIWiTpGldohraxjVrs"
+        self.SECRET_KEY = "6JMBHzW-BN5C2omz2rFDuj9dCDQy3_9F4ty-DY-d"
+        self.BUCKET_NAME = bucket_name
+        self.AUTH = None
+
+        self._get_authentation()
+
+    def _get_authentation(self):
+        self.AUTH  = Auth(self.ACCESS_KEY, self.SECRET_KEY)
+
+    def upload_data(self, key, data):
+        token = self.AUTH.upload_token(self.BUCKET_NAME)
+        ret, info = put_data(token, key, data)
+        return ret, info
+
+    def upload_file(self, key, file_path, mime_type=None):
+        token = self.AUTH.upload_token(self.BUCKET_NAME)
+        ret, info = put_file(token, key, file_path, check_crc=True)
+        return ret, info
+
+class Mongo:
+
+    def __init__(self, db, ip = "127.0.0.0", port = 11277):
+        con = pymongo.Connection()
+        self.db = con[db]
+
+    def insert_data(self, data):
+        self.db.picture.insert(data)
+
+    def search_data(self, condition=None):
+        return self.db.picture.find(condition)
+
+MONGO = Mongo("test")
+QINIU = Qiniu()
--- pidiao/view.py
+++ pidiao/view.py
@@ -2,12 +2,33 @@ from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
 
+from uploader import MONGO
+
 def index(request):
+    data = MONGO.search_data({}).limit(6)
+    picture_list = []
+    for _data in data:
+        d = {}
+        d['url'] = 'http://7xi4me.com1.z0.glb.clouddn.com/%s' % _data["id"]
+        print d['url']
+        d['name'] = _data["id"]
+        d['des'] = _data['des']
+        picture_list.append(d)
+
     t = get_template("index.html")
-    html = t.render(Context({}))
+    html = t.render(Context({"picture_list": picture_list}))
     return HttpResponse(html)
 
 def product_list(request):
+    data = MONGO.search_data({}).limit(6)
+    picture_list = []
+    for _data in data:
+        d = {}
+        d['url'] = 'http://7xi4me.com1.z0.glb.clouddn.com/%s' % _data["id"]
+        print d['url']
+        d['name'] = _data["id"]
+        d['des'] = _data['des']
+        picture_list.append(d)
     t = get_template("list.html")
-    html = t.render(Context({"pagelist": ["p2", "p3", "p4"], "pagenum": 4}))
+    html = t.render(Context({"pagelist": ["p2", "p3", "p4"], "pagenum": 4, "picture_list": picture_list}))
     return HttpResponse(html)
